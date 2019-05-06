9d3482722c8f6facc93521f14eab82dcb4990887
novp19@vse.cz <hoveziprase96>
2019-05-06 17:18:22 +0200

    header a footer

--- app/views/header.php
+++ app/views/header.php
@@ -1,9 +1,11 @@
 <html>
 <head>
     <title>4IZ278</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width = device-width, initial-scale = 1">
     <base href="/~novp19/"/>
     <link rel="stylesheet" href="public/css/styles.css"/>
-    <script src="public/js/jquery.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="public/js/script.js"></script>
 </head>
 <body>
